90cc3e9c820f00500a6d052c3b822932ae9fc791
Henrique Augusto <hick_97@hotmail.com>
2019-10-27 02:08:40 -0300

    Adding auth middleware + User and Session controllers

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,20 @@
 import express from 'express';
 
+/* 
+  CONTROLLERS
+*/
+import UserController from '../app/controllers/UserController';
+import SessionController from '../app/controllers/SessionController';
+// import ToolsController from '../app/controllers/ToolsController';
+
 const routes = express.Router();
 
-routes.get('/', (req, res) => {
-  return res.send('OK');
-});
+/* USER */
+
+routes.post('/user', UserController.store);
+
+/* SESSION */
+
+routes.post('/session', SessionController.store);
 
 export default routes;
